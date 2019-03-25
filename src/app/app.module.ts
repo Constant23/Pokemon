27653a13d8717f5bd82d40fdbf1908aec634e0a6
Langnito Constant <constant.langnito@etudiant.univ-rennes1.fr>
2019-03-25 17:18:33 +0100

    Accès à une API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+
+import { FormsModule } from '@angular/forms';
+import {FilterPokemonPipe} from './filter-pokemon.pipe';
+import {HttpClientModule} from '@angular/common/http';
+import {PokemonService} from './pokemon.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    FilterPokemonPipe
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
